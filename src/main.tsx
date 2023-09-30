39b701df1a45d0cb75e0497b92acfaad567ac28b
PinkyPie <danilovich36misha@gmail.com>
2023-09-30 22:55:02 +0300

    Reset global styles and add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from '@/containers';
+import { GlobalStyles } from '@/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>
 );
---
+++ src/theme/GlobalStyles.tsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    font-family: 'Martian Mono', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  & * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  & button {
+    color: currentColor;
+    border: none;
+    cursor: pointer;
+    background: transparent;
+  }
+`;
